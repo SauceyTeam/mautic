aaca0773284440488c80a97322a173281d4bed71
justinkiang <justin@justinkiang.com>
2025-06-23 14:00:57 -0700

    Refactor form.html.twig to enhance structure and readability

---
+++ app/config/config_template.php
@@ -0,0 +1,16 @@
+<?php
+$parameters = array(
+        'db_driver' => 'pdo_mysql',
+        'db_host' => '{{db_host}}',
+        'db_port' => '{{db_port}}',
+        'db_name' => '{{db_name}}',
+        'db_user' => '{{db_user}}',
+        'db_password' => '{{db_password}}',
+        'db_table_prefix' => null,
+        'db_backup_tables' => 0,
+        'db_backup_prefix' => 'bak_',
+        'db_host_ro' => null,
+        'secret_key' => '{{secret_key}}',
+        'site_url' => '{{site_url}}',
+        'api_enabled' => 1,
+);
